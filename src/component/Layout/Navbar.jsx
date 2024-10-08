3c14f1ca8c3c12ca8d00ec4da4b90a95f775b843
unknown <robiulhassanrobi33@gmail.com>
2024-10-08 17:53:03 +0600

    Navbar fix

--- src/component/Layout/Navbar.jsx
+++ src/component/Layout/Navbar.jsx
@@ -3,47 +3,90 @@ import { Link } from "react-router-dom";
 import { FaBarsStaggered } from "react-icons/fa6";
 
 const Navbar = () => {
-  const [show, setShow] = useState(false);
+  const [open, setOpen] = useState(false);
   return (
-    <nav className="relative">
-      <div className="container py-5  flex items-center justify-between">
-        <Link to="/" className="w-1/4">
-          <img src="/logo.png.png" alt="logo" />
-        </Link>
-       <button onClick={() => setShow(!show)} className="block md:hidden">
-       <FaBarsStaggered />
-       </button>
-        <ul className={`absolute top-full left-0 md:static bg-white h-screen md:h-auto w-full md:w-fit flex flex-col md:flex-row items-center gap-10 font-medium font-primary text-primary transition-all ${
-          show ? "translate-x-0" : "-translate-x-full md:translate-x-0" }`}>
-          <li>
-            <Link to="/" className="transition-all hover:text-secondery">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" className="transition-all hover:text-secondery">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/layouts" className="transition-all hover:text-secondery">
-              Portfolio Layouts
-            </Link>
-          </li>
-          <li>
-            <Link to="/portfoliosingle" className="transition-all hover:text-secondery">
-              Portfolio Single
-            </Link>
-          </li>
-          <li>
-            <Link to="/blog" className="transition-all hover:text-secondery">
-              Blog
-            </Link>
-          </li>
-        </ul>
+      <header className={`flex w-full items-center bg-white dark:bg-dark`}>
+      <div className="container">
+        <div className="relative -mx-4 flex items-center justify-between">
+          <div className="w-60 max-w-full px-4">
+            <a href="/#" className="block w-full py-5">
+              <img
+               src="/logo.png.png"
+                alt="logo"
+                className="dark:hidden"
+              />
+              <img
+                src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo-white.svg"
+                alt="logo"
+                className="hidden dark:block"
+              />
+            </a>
+          </div>
+          <div className="flex w-full items-center justify-between px-4">
+            <div>
+              <button
+                onClick={() => setOpen(!open)}
+                id="navbarToggler"
+                className={` ${
+                  open && "navbarTogglerActive"
+                } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
+              >
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color bg-black"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color bg-black"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color bg-black"></span>
+              </button>
+              <nav
+                // :className="!navbarOpen && 'hidden' "
+                id="navbarCollapse"
+                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none lg:dark:bg-transparent ${
+                  !open && "hidden"
+                } `}
+              >
+                <ul className="block lg:flex">
+                  <ListItem NavLink="/">Home</ListItem>
+                  <ListItem NavLink="/about">About</ListItem>
+                  <ListItem NavLink="/portfoliosingle">Portfolio Layouts</ListItem>
+                  <ListItem NavLink="/layouts">Portfolio Single</ListItem>
+                  <ListItem NavLink="/blog">Blog</ListItem>
+                </ul>
+              </nav>
+            </div>
+            <div className="hidden justify-end pr-16 sm:flex lg:pr-0">
+              <a
+                href="/#"
+                className="px-7 py-3 text-base font-medium text-dark hover:text-primary dark:text-white"
+              >
+                Sign in
+              </a>
+
+              <a
+                href="/#"
+                className="rounded-md bg-primary px-7 py-3 text-base font-medium text-white hover:bg-primary/90"
+              >
+                Sign Up
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </nav>
+    </header>
   );
 };
 
 export default Navbar;
+
+
+const ListItem = ({ children, NavLink }) => {
+  return (
+    <>
+      <li>
+        <a
+          href={NavLink}
+          className="flex py-2 text-base font-medium text-body-color hover:text-dark dark:text-dark-6 dark:hover:text-white lg:ml-12 lg:inline-flex"
+        >
+          {children}
+        </a>
+      </li>
+    </>
+  );
+};
